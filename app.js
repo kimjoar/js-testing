b5c1f9ef9abd8418c3d2716582b80d0e8c8db1af
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-09 18:59:10 +0200

    first integration test

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 (function(Simple) {
 
-  BEKK.User = Simple.Model.extend({});
+  BEKK.User = Simple.Model.extend({
+      initialize: function(options) {
+          if (options && options.screen_name) {
+            this.name = options.screen_name;
+            this.url = "https://api.twitter.com/1/users/show.json?screen_name=" + this.name + "&include_entities=true";
+          }
+      }
+  });
 
   BEKK.UserView = Simple.View.extend({
 
--- test/spec/UserIntegrationSpec.js
+++ test/spec/UserIntegrationSpec.js
@@ -1,7 +1,14 @@
 describe("Integration tests", function(){
 
-    xit("should fetch data from Twitter and populate the view", function(){
+    it("should fetch data from Twitter and populate the view", function(){
+        var user = new BEKK.User({ screen_name: "kimjoar", el: $('<div></div>') });
 
+        user.fetch(function(data) {
+            var view = new BEKK.UserView({ data: data });
+            view.render();
+
+            expect(true).toBeFalsy();
+        });
     });
 
     xit("should fetch data and populate view", function() {
