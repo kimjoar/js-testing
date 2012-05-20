7b063b897055989f7bbc7ca992fa9f584c03f5b6
Tine Kleivane <kleivane@gmail.com>
2012-05-20 21:36:37 +0200

    testing the fetch-method and the generation of correct events

--- test/spec/UserIntegrationSpec.js
+++ test/spec/UserIntegrationSpec.js
@@ -49,6 +49,33 @@ describe("Integration tests", function(){
 
     });
 
+    it("should trigger fetch:finished on fetch and populate the model with data", function(){
+       
+
+        this.server = sinon.fakeServer.create();
+        this.server.respondWith([200, {},
+                                 '{"id":20823739,"id_str":"20823739","name":"Kim Joar Bekkelund","location":"Oslo, Norway"}']);
+        
+        
+        var user = new BEKK.User({screen_name: "kimjoar"});
+        var spy = sinon.spy();
+        var base = this;
+        
+        user.fetch();  
+        user.on("fetch:finished", spy); 
+
+        this.server.respond();
+        expect(user.attr("id")).toEqual(20823739);
+        expect(user.attr("id_str")).toEqual("20823739");
+        expect(user.attr("name")).toEqual("Kim Joar Bekkelund");
+        expect(user.attr("location")).toEqual("Oslo, Norway");
+
+        expect(spy.called).toBeTruthy();
+
+
+        this.server.restore();
+    });
+
     it("on fetch:finished the view should render with correct name", function() {
         var user = new BEKK.User({screen_name: "kimjoar"});
         user.attr("name", "Kim Joar Bekkelund");
