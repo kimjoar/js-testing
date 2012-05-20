64544dc273b522666acd0471d2feea724b79af4d
Tine Kleivane <kleivane@gmail.com>
2012-05-20 11:51:25 +0200

    Den første testen mot twitter kjører. OBS: Krever endringer i simple.js

--- test/spec/UserIntegrationSpec.js
+++ test/spec/UserIntegrationSpec.js
@@ -1,17 +1,30 @@
 describe("Integration tests", function(){
 
     it("should fetch data from Twitter and populate the view", function(){
-        var user = new BEKK.User({ screen_name: "kimjoar", el: $('<div></div>') });
+        // Tine: I den orginale testen var det noe mystisk, den bør feile pga cross-domain request med json, men den 
+        //passerer helt fint i min specrunner:(
+        runs(function(){
+            var user = new BEKK.User({ screen_name: "kimjoar"});
+            this.view = {};
 
-        user.fetch(function(data) {
-            var view = new BEKK.UserView({ data: data });
-            view.render();
-
-            expect(true).toBeFalsy();
+            var base = this;
+            user.fetch(function(data) {
+                base.view = new BEKK.UserView({ user: data, el: $('<div></div>')  });
+                base.view.render();
+            });
+            
         });
+
+        waits(500);
+
+        runs(function(){
+            expect(this.view).toContainInTemplate("Kim Joar");
+        });
+        
+       
     });
 
-    xit("should fetch data and populate view", function() {
+    it("should fetch data and populate view", function() {
 
     });
 
