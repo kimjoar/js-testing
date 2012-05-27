c7fa235a2168fde1afb0cd8ad421fd7df23f6bbe
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-27 17:06:21 +0200

    Added start method to app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 (function(Simple) {
 
+  BEKK.start = function() {
+    // called when the app can be started
+  };
+
   BEKK.User = Simple.Model.extend({
-      initialize: function(options) {
-          if (options && options.screen_name) {
-            this.name = options.screen_name;
-            this.url = "https://api.twitter.com/1/users/show.json?screen_name=" + this.name + "&include_entities=true";
-          }
+    initialize: function(options) {
+      if (options && options.screen_name) {
+        this.name = options.screen_name;
+        this.url = "https://api.twitter.com/1/users/show.json?screen_name=" + this.name + "&include_entities=true";
       }
+    }
   });
 
   BEKK.UserView = Simple.View.extend({
@@ -14,26 +18,26 @@
     template: '<div><h1 class="name">Hei {{name}}!</h1></div>',
 
     initialize: function(options) {
-        this.user = options.user;
-        
-        var base = this;
-        if (this.user instanceof Simple.Model) {
-          this.user.on("fetch:finished", function(){
-              base.render();
-          })
-      };
+      this.user = options.user;
+
+      var base = this;
+      if (this.user instanceof Simple.Model) {
+        this.user.on("fetch:finished", function(){
+          base.render();
+        });
+      }
     },
 
     render: function() {
-        var data;
+      var data;
 
-        if (this.user instanceof Simple.Model) {
-            data = this.user.toJSON();
-        } else {
-            data = this.user;
-        }
+      if (this.user instanceof Simple.Model) {
+        data = this.user.toJSON();
+      } else {
+        data = this.user;
+      }
 
-        this.el.html(Mustache.to_html(this.template, data));
+      this.el.html(Mustache.to_html(this.template, data));
     }
   });
 
