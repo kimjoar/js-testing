c9cab428c1ff9fea4ec9b30e26332addc98956c1
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 00:19:12 +0200

    up and running

--- app.js
+++ app.js
@@ -4,7 +4,15 @@
 
   BEKK.start = function() {
     // called when the app can be started
+    var newStatusView = new BEKK.NewStatusView({ el: $("#status-form") });
+    newStatusView.render();
+
+    var monologs = new BEKK.Monologs();
+    var statusesView = new BEKK.StatusesView({ el: $("#monologs"), monologs: monologs });
+    statusesView.render();
+
     var user = new BEKK.User({ screen_name: "kimjoar" });
+
     var userView = new BEKK.UserView({ el: $("#profile"), user: user });
     user.fetch({
       dataType: "jsonp",
@@ -14,12 +22,35 @@
     });
   };
 
+  BEKK.Monologs = Simple.Model.extend({
+    initialize: function() {
+      Simple.events.on("new-status", this.add, this);
+      this.attr("monologs", []);
+    },
+
+    add: function(status) {
+      var monologs = this.attr("monologs");
+      monologs.push(status);
+      this.attr("monologs", monologs);
+      this.trigger("add");
+      Simple.events.trigger("status:added");
+    }
+  });
+
   BEKK.User = Simple.Model.extend({
     initialize: function(options) {
       if (options && options.screen_name) {
         this.name = options.screen_name;
         this.url = "https://api.twitter.com/1/users/show.json?screen_name=" + this.name + "&include_entities=true";
+
+        Simple.events.on("status:added", this.statusAdded, this);
       }
+    },
+
+    statusAdded: function() {
+      var count = this.attr("statuses_count") + 1;
+      this.attr("statuses_count", count);
+      this.trigger("change");
     }
   });
 
@@ -38,9 +69,8 @@
 
       var base = this;
       if (this.user instanceof Simple.Model) {
-        this.user.on("fetch:finished", function(){
-          base.render();
-        });
+        this.user.on("fetch:finished", this.render, this);
+        this.user.on("change", this.render, this);
       }
     },
 
@@ -58,9 +88,42 @@
   });
 
   BEKK.NewStatusView = Simple.View.extend({
+    template: '<form id="new-status" action="#">' +
+        '<label for="status_text">Status</label>' +
+        '<textarea id="status_text" name="status" placeholder="Hva tenker du nå?"></textarea>' +
+        '<button type="submit" class="btn">Post oppdatering</button>' +
+      '</form>',
+
+    events: {
+      "submit form": "newStatus"
+    },
+
+    render: function() {
+      this.el.html(Mustache.to_html(this.template));
+    },
+
+    newStatus: function(event) {
+      event.preventDefault();
+      var status = this.DOM("#status_text").val();
+      Simple.events.trigger("new-status", status);
+      this.DOM("#status_text").val("");
+    }
   });
 
   BEKK.StatusesView = Simple.View.extend({
+    template: '<h2>Oppdateringer</h2>' +
+      '<ul>' +
+      '{{#monologs}}<li>{{.}}</li>{{/monologs}}' +
+      '</ul>',
+
+    initialize: function(options) {
+      this.monologs = options.monologs;
+      this.monologs.on("add", this.render, this);
+    },
+
+    render: function() {
+      this.el.html(Mustache.to_html(this.template, this.monologs.toJSON()));
+    }
   });
 
 })(Simple, Mustache);
