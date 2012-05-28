b705e52ea8ca1fdd434519e1dcbaf9b706ccd05d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 16:37:50 +0200

    Removed toContainInSelector

--- test/helper/customMatchers.js
+++ test/helper/customMatchers.js
@@ -2,11 +2,6 @@ jasmine.Matchers.prototype.toContainInDOM = function(text) {
   return this.actual.el.html().indexOf(text) !== -1;
 };
 
-//Her er det en feil/bug
-jasmine.Matchers.prototype.toContainInSelector = function(selector, text) {
-  return this.actual.DOM(selector).text().indexOf(text) !== -1;
-};
-
 jasmine.Matchers.prototype.toBeEmpty = function() {
   return this.actual.length === 0;
 };
