fe3edd395b73d52b1e0e80351c05a6789cb3b277
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 10:07:02 +0200

    Monologs test is running

--- test/helper/testhelper.js
+++ test/helper/testhelper.js
@@ -1,3 +1,7 @@
 jasmine.Matchers.prototype.toContainInDOM = function(text) {
-	return this.actual.el.html().indexOf(text) !== -1;
+  return this.actual.el.html().indexOf(text) !== -1;
+};
+
+jasmine.Matchers.prototype.toBeEmpty = function() {
+  return this.actual.length === 0;
 };
--- test/spec/monologsSpec.js
+++ test/spec/monologsSpec.js
@@ -1,8 +1,37 @@
 describe("Monologs", function() {
   describe("add", function() {
-    it("appends one item", function() {
+    it("appends an item", function() {
       var monologs = new BEKK.Monologs();
-      expect(monologs.attr("monologs")).toBeEmpty();
+      monologs.add("test1");
+      monologs.add("test2");
+      expect(monologs.attr("monologs")).toEqual(["test1", "test2"]);
+    });
+
+    it("adds a monolog on 'new-status' event", function() {
+      var monologs = new BEKK.Monologs();
+      Simple.events.trigger("new-status", "test");
+      expect(monologs.attr("monologs")).toEqual(["test"]);
+    });
+
+    it("triggers 'event' when monolog added", function() {
+      var monologs = new BEKK.Monologs();
+      var spy = sinon.spy();
+      monologs.on("add", spy);
+      monologs.add("test");
+      expect(spy).toHaveBeenCalledOnce();
+    });
+  });
+
+  describe("count", function() {
+    it("returns 0 for number of monologs when no monologs added", function() {
+      var monologs = new BEKK.Monologs();
+      expect(monologs.count()).toEqual(0);
+    });
+
+    it("returns 1 for number of monologs when one monolog added", function() {
+      var monologs = new BEKK.Monologs();
+      monologs.add("test");
+      expect(monologs.count()).toEqual(1);
     });
   });
 });
