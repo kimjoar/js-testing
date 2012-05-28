bdc155887b605a97a83279549d4ecda5d7a806c1
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 03:38:10 +0200

    all tests running

--- test/spec/UserIntegrationSpec.js
+++ test/spec/UserIntegrationSpec.js
@@ -1,24 +1,21 @@
-xdescribe("Integration tests", function(){
+describe("Integration tests", function(){
 
-    xit("should fetch data from Twitter and populate the view", function(){
-        // Tine: I den orginale testen var det noe mystisk, den bør feile pga cross-domain request med json, men den 
-        //passerer helt fint i min specrunner:(
-        //OBS: jsonp trengs i simple.js
+    it("should fetch data from Twitter and populate the view", function(){
+        var view;
+
         runs(function(){
             var user = new BEKK.User({ screen_name: "kimjoar"});
-            this.view = {};
+            view = new BEKK.UserView({ user: user, el: $('<div></div>') });
 
-            var base = this;
-            user.fetch(function(data) {
-                base.view = new BEKK.UserView({ user: data, el: $('<div></div>')  });
-                base.view.render();
+            user.fetch(function() {
+                view.render();
             });
         });
 
         waits(1000);
 
         runs(function(){
-            expect(this.view).toContainInDOM("Kim Joar");
+            expect(view).toContainInDOM("Kim Joar");
         });
     });
 
@@ -27,40 +24,40 @@ xdescribe("Integration tests", function(){
 
         this.server = sinon.fakeServer.create();
         this.server.respondWith([200, {},
-                                 '{"name":"Kim Joar Bekkelund","screen_name":"kimjoar"}']);
-
+          '{"name":"Kim Joar Bekkelund","screen_name":"kimjoar"}']);
 
         var user = new BEKK.User({screen_name: "kimjoar"});
+        user.dataType = "json";
 
-        var base = this;
-        var callback = function(data){
-            base.view = new BEKK.UserView({ user: data, el: $('<div></div>')  });
-            base.view.render();
-        };
-        
-        user.fetch(callback);   
+        var view = new BEKK.UserView({ user: user, el: $('<div></div>')  });
+
+        user.fetch(function(){
+            view.render();
+        });
 
         this.server.respond();
-        expect(this.view).toContainInDOM("Kim Joar");
+        expect(view).toContainInDOM("Kim Joar");
         this.server.restore();
 
     });
 
 	//Ønsker vi å beholde denne testen. Den tester mer rammeverket enn vår egen app, men vi sikrer oss at fetch:finished gjør det den skal.
     it("should trigger fetch:finished on fetch and populate the model with data", function(){
-       
+
         //Er dette mere testing av rammeverk? Tester at modellen blir populert rett slik at neste test kan populere modellen selv.
         this.server = sinon.fakeServer.create();
         this.server.respondWith([200, {},
-                                 '{"id":20823739,"id_str":"20823739","name":"Kim Joar Bekkelund","location":"Oslo, Norway"}']);
-        
-        
+          '{"id":20823739,"id_str":"20823739","name":"Kim Joar Bekkelund","location":"Oslo, Norway"}']);
+
+
         var user = new BEKK.User({screen_name: "kimjoar"});
+        user.dataType = "json";
+
         var spy = sinon.spy(user, "trigger");
         var base = this;
-        
-        user.fetch();  
-        
+
+        user.fetch();
+
         this.server.respond();
         expect(user.attr("id")).toEqual(20823739);
         expect(user.attr("id_str")).toEqual("20823739");
@@ -79,7 +76,7 @@ xdescribe("Integration tests", function(){
     it("on fetch:finished the view should render with correct name", function() {
         var user = new BEKK.User({screen_name: "kimjoar"});
         user.attr("name", "Kim Joar Bekkelund");
-        
+
         this.view = new BEKK.UserView({ user: user, el: $('<div></div>')  });
 
         user.trigger("fetch:finished");
@@ -87,10 +84,6 @@ xdescribe("Integration tests", function(){
         expect(this.view).toContainInDOM("Kim Joar");
 
     });
-	
-	it("", function(){
-		
-	})
 
 });
 
