7120171853ef4ef4a5c113438c7fc110f48dd207
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 20:11:09 +0200

    added missing test to statusesViewSpec

--- test/spec/statusesViewSpec.js
+++ test/spec/statusesViewSpec.js
@@ -1,4 +1,11 @@
 describe("the statuses view", function() {
+  xit("should show all monologs", function() {
+    var monologs = new BEKK.Monologs();
+    monologs.attr("monologs", ["Status 1", "Status 2"]);
+
+    // ...
+  });
+
   xit("should update view when a monolog is added", function() {
   });
 });
