1ff836b2b551d228d1365f31466ffe20aa107ead
Tine Kleivane <kleivane@gmail.com>
2012-05-28 14:27:27 +0200

    Added new test and testhelper for showing static monologs in statusesView.

--- test/helper/testhelper.js
+++ test/helper/testhelper.js
@@ -2,6 +2,10 @@ jasmine.Matchers.prototype.toContainInDOM = function(text) {
   return this.actual.el.html().indexOf(text) !== -1;
 };
 
+jasmine.Matchers.prototype.toContainInSelector = function(selector, text) {
+  return this.actual.DOM(selector).text().indexOf(text) !== -1;
+};
+
 jasmine.Matchers.prototype.toBeEmpty = function() {
   return this.actual.length === 0;
 };
--- test/spec/statusesViewSpec.js
+++ test/spec/statusesViewSpec.js
@@ -1,4 +1,17 @@
 describe("the statuses view", function() {
+
+  it("should show all monologs", function() {
+    var monologs = new BEKK.Monologs();
+    monologs.attr("monologs", ["Status 1", "Status 2"]);
+
+    var view = new BEKK.StatusesView({ el: $("<div></div>"), monologs: monologs });
+    view.render();
+    
+    expect(view.DOM("li").length).toEqual(2);
+    expect(view).toContainInSelector("li", "Status 1");
+    expect(view).toContainInSelector("li", "Status 2");
+  });
+
   it("should update view when a monolog is added", function() {
     var monologs = new BEKK.Monologs();
     var view = new BEKK.StatusesView({ el: $("<div></div>"), monologs: monologs });
@@ -6,5 +19,6 @@ describe("the statuses view", function() {
     expect(view.DOM("li").length).toEqual(0);
     monologs.add("test");
     expect(view.DOM("li").length).toEqual(1);
+    expect(view).toContainInSelector("li", "test");
   });
 });
