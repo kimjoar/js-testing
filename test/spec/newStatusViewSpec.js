be6e9934e91eeefa70f4a52c9bdfa18fba91da86
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 14:22:59 +0200

    make new status view spec easier to read

--- test/spec/newStatusViewSpec.js
+++ test/spec/newStatusViewSpec.js
@@ -7,16 +7,21 @@ describe("The new status view", function() {
     this.view.render();
     this.view.input("test");
     this.view.DOM("form").submit();
+
     expect(this.view.input()).toEqual("");
   });
 
   it("should trigger 'new-status' with input value when submitted", function() {
     var spy = sinon.spy();
     Simple.events.on("new-status", spy);
+
     this.view.render();
     this.view.input("test");
     this.view.DOM("form").submit();
+
     expect(spy).toHaveBeenCalledOnce();
     expect(spy).toHaveBeenCalledWith("test");
+
+    Simple.events.off("new-status", spy);
   });
 });
