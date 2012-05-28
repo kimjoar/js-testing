7d7a85c983167f94747ba7a2fd7ed08701fc6069
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-28 10:21:57 +0200

    moved monologs initialization to beforeEach

--- test/spec/monologsSpec.js
+++ test/spec/monologsSpec.js
@@ -1,37 +1,36 @@
 describe("Monologs", function() {
+  beforeEach(function() {
+    this.monologs = new BEKK.Monologs();
+  });
+
   describe("add", function() {
     it("appends an item", function() {
-      var monologs = new BEKK.Monologs();
-      monologs.add("test1");
-      monologs.add("test2");
-      expect(monologs.attr("monologs")).toEqual(["test1", "test2"]);
+      this.monologs.add("test1");
+      this.monologs.add("test2");
+      expect(this.monologs.attr("monologs")).toEqual(["test1", "test2"]);
     });
 
     it("adds a monolog on 'new-status' event", function() {
-      var monologs = new BEKK.Monologs();
       Simple.events.trigger("new-status", "test");
-      expect(monologs.attr("monologs")).toEqual(["test"]);
+      expect(this.monologs.attr("monologs")).toEqual(["test"]);
     });
 
     it("triggers 'event' when monolog added", function() {
-      var monologs = new BEKK.Monologs();
       var spy = sinon.spy();
-      monologs.on("add", spy);
-      monologs.add("test");
+      this.monologs.on("add", spy);
+      this.monologs.add("test");
       expect(spy).toHaveBeenCalledOnce();
     });
   });
 
   describe("count", function() {
     it("returns 0 for number of monologs when no monologs added", function() {
-      var monologs = new BEKK.Monologs();
-      expect(monologs.count()).toEqual(0);
+      expect(this.monologs.count()).toEqual(0);
     });
 
     it("returns 1 for number of monologs when one monolog added", function() {
-      var monologs = new BEKK.Monologs();
-      monologs.add("test");
-      expect(monologs.count()).toEqual(1);
+      this.monologs.add("test");
+      expect(this.monologs.count()).toEqual(1);
     });
   });
 });
