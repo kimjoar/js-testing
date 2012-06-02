bef48265cff7f0634a06192e4a34908c7a2a34a7
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-06-02 15:16:21 +0200

    removed unnecessary tests, we will refactor our first test instead

--- test/spec/userViewSpec.js
+++ test/spec/userViewSpec.js
@@ -8,38 +8,12 @@ describe("The user view", function(){
         // expect(view.user).toBeDefined();
     });
 
-    xit("should show name when rendered", function(){
+    xit("should show user info when rendered", function(){
         var user = {
             name: "Test Testesen"
         };
 
         // ...
-    });
-
-    xit("should show name in heading when rendered", function() {
-        var user = {
-            name: "Test Testesen"
-        };
-
-        // ...
-    });
-
-    xit("should render model", function() {
-        var user = new BEKK.User({ screen_name: "kimjoar" });
-
-        // ...
-    });
-
-    xit("should fetch data from Twitter and populate the view", function(){
-      // runs
-      // waits
-      // runs
-    });
-
-    xit("should populate the view when data is fetched", function() {
-    });
-
-    xit("should populate the view when data is fetched", function(){
     });
 
     xit("should update view when monolog is added", function() {
